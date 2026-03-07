c56b8cb5b860f415d16c58b46fae0cb89f994901
DidaЯ <didardmr2004@gmail.com>
2026-03-07 05:06:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function changeLang(lang) {
+/*function changeLang(lang) {
     const elements = document.querySelectorAll('.translate');
     
     elements.forEach(el => {
@@ -12,4 +12,19 @@ function changeLang(lang) {
 }
 
 // Konsol Bilgisi
-console.log("Portfolio Security Scan: Success. Version 2.0 Active.");
+console.log("Portfolio Security Scan: Success. Version 2.0 Active.");*/
+
+function changeLang(lang) {
+    localStorage.setItem('preferredLang', lang); // Dili kaydet
+    const elements = document.querySelectorAll('.translate');
+    elements.forEach(el => {
+        const text = el.getAttribute('data-' + lang);
+        if (text) el.textContent = text;
+    });
+}
+
+// Sayfa yüklendiğinde kaydedilen dili uygula
+window.onload = () => {
+    const savedLang = localStorage.getItem('preferredLang') || 'en';
+    changeLang(savedLang);
+};
